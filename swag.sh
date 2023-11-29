9ce3286ba0e485e560c77953145025f535540010
ib31iat <rtobias286@gmail.com>
2023-11-29 18:50:35 +0000

    Update swag bash script

--- swag.sh
+++ swag.sh
@@ -3,22 +3,24 @@
 python3 swag.py \
         --dir=".cache/" \
         --dataset="STEAD" \
-        --dataset_path="/data/STEAD_dataset/.seisbench/datasets/stead/" \
-        --batch_size=1024 \
+        --batch_size=512 \
         --model="EQTransformer" \
+        --dataset_path="/data/STEAD_dataset/.seisbench/datasets/stead/" \
         --epochs=20 \
         --save_freq=5 \
-        --eval_freq=1 \
-        --lr_init=0.01 \
+        --eval_freq=10 \
+        --lr_init=0.001 \
         --momentum=0.9 \
         --wd=1e-4 \
         --swa \
-        --swa_start=1 \
+        --swa_start=15 \
         --swa_lr=0.02 \
         --cov_mat \
         --max_num_models=20 \
         --loss="CE" \
         --seed=42 \
-        --num_workers=24
+        --num_workers=24 \
+        --no_schedule \
+        --verbose
 
-# unused option: --no_schedule, --resumte="T", --swa_resume="T"
+# unused option: , --resumte="T", --swa_resume="T"
