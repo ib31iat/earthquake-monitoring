218ef6483666be25d3e073e71bf4785d31ef6e10
Herbsi <git@hoehenberger.me>
2023-12-07 13:32:40 +0100

    Residual histogram (basic)

--- code/utils/visualisations.py
+++ code/utils/visualisations.py
@@ -15,3 +15,22 @@ def confusion_matrix(metrics, ax=None, **kwargs):
     )
     disp.plot(ax=ax, **kwargs)
 
+
+def residual_histogram(metrics, axs=None, **kwargs):
+    p_res = metrics["p_res"] / 100
+    s_res = metrics["s_res"] / 100
+    snr = metrics["snr"]
+
+    p_indices = np.abs(p_res) < 1
+    s_indices = np.abs(s_res) < 1
+
+    axs[0, 0].hist(p_res[p_indices], bins=50)
+    axs[1, 0].hist(s_res[s_indices], bins=50)
+
+    axs[0, 1].set_xscale("log")
+    axs[1, 1].set_xscale("log")
+    axs[0, 1].set_yscale("log")
+    axs[1, 1].set_yscale("log")
+    axs[0, 1].scatter(snr[p_indices][::100], p_res[p_indices][::100])
+    axs[1, 1].scatter(snr[s_indices][::100], s_res[s_indices][::100])
+
