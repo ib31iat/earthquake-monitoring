07fdbf30dd1f502925f08ad905321fde95aeced6
ib31iat <rtobias286@gmail.com>
2023-12-12 20:31:53 +0000

    Add bedretto results (training from scratch)

---
+++ results/trained_models/eqt_bedretto_no_retrain/command.sh
@@ -0,0 +1,1 @@
+swag.py --dir=.cache-bedretto/ --dataset=Bedretto --batch_size=512 --model=EQTransformer --dataset_path=/home/trahn/data/bedretto/ --epochs=50 --save_freq=5 --eval_freq=10 --lr_init=0.001 --momentum=0.9 --wd=1e-4 --swa --swa_start=40 --swa_lr=0.02 --cov_mat --max_num_models=20 --loss=CE --seed=42 --num_workers=24 --no_schedule --verbose --resume .cache-bedretto/checkpoint-35.pt
